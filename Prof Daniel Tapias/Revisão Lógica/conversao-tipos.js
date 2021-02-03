023b8831aa55b292bc0cfdd2a51e0c20502742c8
Felipe dos Santos <felipesantos.dsb@hotmail.com>
2021-02-03 17:24:03 -0300

    Adicionando Conversão de Tipos

---
+++ Prof Daniel Tapias/Revisão Lógica/conversao-tipos.js
@@ -0,0 +1,16 @@
+let n1 = "10";
+n1 = parseFloat(n1);
+let n2 = 2;
+
+console.log(n1 * n2, typeof n1, typeof n2);
+
+//parseInt, parseFloat, Number()
+
+//  CONVERTER NUMBER P/ STRING
+
+n2 = 11;
+n2 = n2.toString(2); //O numero no parenteses é a base
+//quando vazio = Decimal, 2 = binário e assim por diante
+
+
+console.log(n2, typeof n2);
