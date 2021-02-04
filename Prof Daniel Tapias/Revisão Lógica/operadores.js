ea0c673cc8c78f4116ed369d6fd41c57bbb1845f
Felipe dos Santos <felipesantos.dsb@hotmail.com>
2021-02-04 19:04:02 -0300

    Inclusão de operadores lógicos na Revisão de Lógica

---
+++ Prof Daniel Tapias/Revisão Lógica/operadores.js
@@ -0,0 +1,32 @@
+//Operadores Aritméticos:
+// + Soma, - Subtração, * Multiplicação, / Divisão
+// % Resto da Divisão, ** Potência em versões mais novas do JavaScript
+
+let n1 = 10;
+let n2 = 5;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(2 ** 3);
+
+//Operadores de Atribuição
+let n3 = 20; // 20 atribuído ao n3
+// o sinal de igual é o operador que fez essa atribuição.
+
+//n3 = n3 + 15;
+// Vai retornar 35, mas pra simplificar, pode-se usar:
+n3 += 15;
+
+console.log(n3);
+
+//Incremento e Decremento
+let i = 0;
+i++;
+console.log(i);
+
+let d = 0;
+d--;
+console.log(d);
